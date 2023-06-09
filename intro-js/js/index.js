5edee842798d1759e3826c06b3865cf9830b47c3
Rodrigo Ochoa <mayagoitiaro@gmail.com>
2023-06-09 11:22:10 -0600

    intro-js

---
+++ intro-js/js/index.js
@@ -0,0 +1,19 @@
+// SOY UN COMENTARIO DE UNA LÍNEA
+
+/* SOY UN COMENTARIO 
+DE VARIAS LÍNEAS */
+
+/* OBTENER MENSAJES EN LA CONSOLA DE DESARROLLO */
+console.log("Hola desde el archivo JS");
+console.warn("Hola soy una alerta");
+console.error(("Hola soy un error"));
+
+/* SALIDA DE DATOS EN EL DOCUMENTO DE LA PAGINA */
+document.write("Hola mundo!");
+
+/* MENSAJES DE ALERTA EN UN POP UP */
+alert("¡ALERTA!");
+
+// ENTRADA DE DATOS
+var nombre = prompt("Dime tu nombre.");
+alert("Hola " + nombre);
